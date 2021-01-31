6bdaf1fa3b38ddb846d9c70c3a76479c1814593b
monteirovictor <victormonteiromaia95@gmail.com>
2021-01-30 21:15:36 -0300

    filtro create

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -6,6 +6,24 @@ const styles = StyleSheet.create({
         backgroundColor:'#fff',
         alignItems:'center',
         justifyContent:'flex-start'
+    },
+    filter:{
+        flexDirection:'row',
+        width:'100%',
+       justifyContent:'space-around',
+      height:70,
+       alignItems:'center'
+    },
+    filterTextActived:{
+        fontWeight:'bold',
+        fontSize:18,
+        color:'#ee6b26'
+    },
+    filterTextInative:{
+        fontWeight:'bold',
+        fontSize:18,
+        color:'#20295f',
+        opacity:0.5
     }
 });
 
