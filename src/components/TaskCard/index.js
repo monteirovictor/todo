1561613ea6fc00fae5cf0f9f1d6a1bd5cd6e41b2
monteirovictor <victormonteiromaia95@gmail.com>
2021-02-01 23:13:09 -0300

    api

--- src/components/TaskCard/index.js
+++ src/components/TaskCard/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {Text,View,Image,TouchableOpacity} from 'react-native';
 
+//datefns
+import {format} from 'date-fns';
+
 import styles from './styles';
 import iconDefault from '../../assets/default.png';
 
-export default function TaskCard({done}){
+export default function TaskCard({done,title,when}){
     return (
 
         <TouchableOpacity style={[styles.card,done && styles.done]}>
             <View style={styles.cardLeft}>
                 <Image source={iconDefault} style={styles.typeActive}></Image>
-                <Text style={styles.cardTitle}>Hora do Sexo</Text>
+                <Text style={styles.cardTitle}>{title}</Text>
             </View>
             <View style={styles.cardRight}>
-            <Text style={styles.cardDate}>31/01/2021</Text>
-            <Text style={styles.cardTime}>19:01</Text>
+            <Text style={styles.cardDate}>{format(new Date(when),'dd/MM/yyyy')}</Text>
+            <Text style={styles.cardTime}>{format(new Date(when),'HH:mm')}</Text>
             </View>
 
         </TouchableOpacity>
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import {View,Text,TouchableOpacity,ScrollView} from 'react-native';
+import React,{useState,useEffect} from 'react';
+import {View,Text,TouchableOpacity,ScrollView,ActivityIndicator} from 'react-native';
 
 import styles from './styles';
 // COMPONENTES 
@@ -7,12 +7,28 @@ import styles from './styles';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import TaskCard from '../../components/TaskCard';
-
+//conexao api
+import api from '../../services/api';
 
 
 export default function Home(){
     
     const [filter,setFilter]=useState('today'); 
+    const [tasks,setTasks]=useState([]);
+    const [load,setLoad]=useState(false);
+
+    async function loadTask(){
+        setLoad(true);
+        await api.get('/task/filter/all/11:11:11:11:11:11')
+        .then(response=>{
+            setTasks(response.data)
+            setLoad(false);
+           
+        });  
+    }
+    useEffect(()=>{
+        loadTask();
+    },[])
 
     return (
         <View style={styles.container}>
@@ -62,8 +78,18 @@ export default function Home(){
             <View style={styles.title}>
                 <Text style={styles.titleText}>TAREFAS</Text>
             </View>
+        
+
             <ScrollView style={styles.content} contentContainerStyle={{alignItems:'center'}}>
-            <TaskCard done={false}/>
+                {
+                    load
+                    ?
+            <ActivityIndicator color={'#ee6b26'} size={50}></ActivityIndicator>
+            :
+                    tasks.map(t =>(
+                        <TaskCard done={false} title={t.title} when={t.when}/>
+                    ))
+                 }
             </ScrollView>
             
             <Footer icon={'add'} />
